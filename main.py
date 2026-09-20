02347536cb58255a5b018336406f6115b11a959a
Adam <adamrburns@outlook.com>
2026-09-20 01:41:26 -0700

    starter main

--- main.py
+++ main.py
@@ -5,7 +5,25 @@ from bfs import bfs
 from dls import dls
 from ids import ids
 
-def main(): pass
+def get_state(prompt): # will be run for both getting start grid and target grid
+    while True:
+        entry = input(prompt)
+        try:
+            return ast.literal_eval(entry)
+        except:
+            print("Invalid input")
+
+def print_output(): pass # output project required output - can be implemented later
+
+def main(): 
+    start = get_state("Start State: ")
+    goal = get_state("Goal State: ")
+
+    print(type(start), type(start[0]), type(start[0][0]), start) # test list is properly converted
+
+    # prompt for algorithm choice
+
+    # print_output()
 
 if __name__ == "__main__":
     main()
